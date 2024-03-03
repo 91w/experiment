5c7f5f479569b250bb2aa8fde71e7376ae03c395
wgh <wangguohong91@163.com>
2024-03-04 06:19:24 +0800

    雏形

---
+++ backend/src/main/java/com/wgh/dao/AdminOperationMapper.java
@@ -0,0 +1,17 @@
+package com.wgh.dao;
+
+import com.wgh.pojo.AdminOperation;
+
+public interface AdminOperationMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(AdminOperation record);
+
+    int insertSelective(AdminOperation record);
+
+    AdminOperation selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(AdminOperation record);
+
+    int updateByPrimaryKey(AdminOperation record);
+}
